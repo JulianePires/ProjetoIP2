e2ca43e260eec8344020c8504ef4f0ef260ced8a
juliane <juliane@juliane-netmake>
2019-11-28 12:43:28 -0300

    Classe de teste

--- POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/LoginPaneController.java
+++ POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/LoginPaneController.java
@@ -1,19 +1,11 @@
 package br.ufrpe.sistema_locadora.gui.controller;
 
-import java.awt.TextField;
-import javax.swing.text.html.ImageView;
 import javafx.scene.layout.AnchorPane;
-import br.ufrpe.sistema_locadora.gui.Sistema;
-import br.ufrpe.sistema_locadora.negocio.FachadaLocadora;
-import demo.Initializable;
 
-public class LoginPaneController extends AnchorPane implements Initializable {
-	private Sistema sistema;
+public class LoginPaneController extends AnchorPane	implements Application{
 	
-	private FachadaLocadora fachada;
 	
-	@FXML private TextField idLogin;
-	
+
 	
 	
 }
---
+++ POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/teste/TesteDados.java
@@ -0,0 +1,16 @@
+package br.ufrpe.sistema_locadora.gui.teste;
+
+public class TesteDados {
+
+	public static void main(String[] args) {
+		//Cadastro Usuarios
+		//Cadastro Clientes
+		//Cadastro Reboques
+		//Cadastro Manutencoes
+		//Cadastro Locacao
+		//Cadastro Reserva
+		
+
+	}
+
+}
