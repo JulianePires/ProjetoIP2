b58bb41ce9d02fcdb3ab28238be8fafe5302f3b2
juliane <juliane@juliane-netmake>
2019-11-29 12:39:01 -0300

    Atualizacao de Views

--- POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/ClienteEditController.java
+++ POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/ClienteEditController.java
@@ -1,5 +1,27 @@
 package br.ufrpe.sistema_locadora.gui.controller;
 
-public class ClienteEditController {
+import br.ufrpe.sistema_locadora.gui.Sistema;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-}
+public class ClienteEditController extends AnchorPane implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+    
+   
+
+}   
+
--- POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/LoginPaneController.java
+++ POO - Locadora Raios de Ouro/src/br/ufrpe/sistema_locadora/gui/controller/LoginPaneController.java
@@ -1,8 +1,18 @@
 package br.ufrpe.sistema_locadora.gui.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-public class LoginPaneController extends AnchorPane	implements Application{
+public class LoginPaneController extends AnchorPane	implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 
